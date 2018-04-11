493828e3671b654261390c8b79958ba20ff737f8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-11 12:40:31 +0000

    Deploy LuTri/AVRClock to github.com/LuTri/AVRClock.git:gh-pages

--- search/all_1.js
+++ search/all_1.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['callback_5fand_5fnext',['callback_and_next',['../customtimer_8c.html#a07b224b7ccf749d8facbab230cf950fc',1,'customtimer.c']]],
   ['check_5fand_5finc_5fsteps',['check_and_inc_steps',['../customtimer_8c.html#aebdb57deac5566dacd4e8731b14af18f',1,'customtimer.c']]],
+  ['cli',['cli',['../test_8c.html#a4229a9b84f8b8673326f12bd41acd27c',1,'cli():&#160;test.c'],['../test_8h.html#a4229a9b84f8b8673326f12bd41acd27c',1,'cli():&#160;test.c']]],
   ['collect_5ftests',['collect_tests',['../test_8c.html#a3e3ddb787ad2dbb29b4088cd227c2897',1,'test.c']]],
   ['compare_5fcb',['compare_cb',['../test_8c.html#ab05c9f82398650ff089739ca88bf1cf3',1,'test.c']]],
   ['compare_5fdec',['compare_dec',['../test_8c.html#abe811f2e100b6cfdf5247cfde29d25e0',1,'test.c']]],
